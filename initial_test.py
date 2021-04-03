a420634438ffa96977687e1528a9f426250fff22
Reese <rgrimsle@andrew.cmu.edu>
2021-04-03 17:56:39 -0400

    add minimal starter code

---
+++ initial_test.py
@@ -0,0 +1,14 @@
+import os, sys, math
+
+from rak811 import Mode Rak811
+import serial
+
+def test_rx(frequency):
+    pass
+
+
+def main():
+    test_rx(930000000)
+
+if __init__ == "__main__":
+    main()
