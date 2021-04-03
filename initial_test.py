a7b1fa59c62690628e6e6bcb3ad3bb25d8753747
Reese <rgrimsle@andrew.cmu.edu>
2021-04-03 18:16:04 -0400

    add example code from rak811 repo

--- initial_test.py
+++ initial_test.py
@@ -1,14 +1,20 @@
-import os, sys, math
+import os, sys, math, time
 
-from rak811 import Mode Rak811
+from rak811 import Mode, Rak811
 import serial
 
 def test_rx(frequency):
     pass
 
+    lora = Rak811()
+    lora.hard_reset()
+    lora.mode = Mode.
+
+
 
 def main():
+    print('main')
     test_rx(930000000)
 
-if __init__ == "__main__":
+if __name__ == "__main__":
     main()
