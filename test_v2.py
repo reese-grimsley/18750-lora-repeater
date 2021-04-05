db97f0dc0beee63c824ee9f16fbcf1975aeee3ab
Reese <rgrimsle@andrew.cmu.edu>
2021-04-05 11:22:09 -0400

    more comprehensive test

--- test_v2.py
+++ test_v2.py
@@ -17,7 +17,7 @@ lora._serial.send_command('help')
 
 
 resp = lora.get_info()
-print(resp)
+# print(resp)
 for x in resp:
     print('\t',x)
 
@@ -27,7 +27,7 @@ print('try to set configuration modes for LoRa p2p')
 
 lora.set_config('lora:work_mode:1')
 resp = lora.get_info()
-print(resp)
+# print(resp)
 for x in resp:
     print('\t',x)
 
@@ -35,13 +35,13 @@ for x in resp:
 print('try to set self as receiver')
 lora.set_confg('lorap2p:transfer_mode:1')
 resp = lora.get_info()
-print(resp)
+# print(resp)
 for x in resp:
     print('\t',x)
 
 
 
-lora.set_confg('lorap2p:915000000:10:0:1:8:16')
+lora.set_config('lorap2p:915000000:10:0:1:8:16')
 resp = lora.get_info()
 print(resp)
 for x in resp:
@@ -52,10 +52,12 @@ print('print recv events')
 i=0
 while True:
     print('loop iter %d' % i)
-    x = lora.get_event(timeout=5)
-    y = lora.get_response(timeout=1)
+    event = lora.get_event(timeout=5)
+    for x in event:
+        print('\t',x)
 
-    print(x)
-    print(y)
+    resp = lora.get_response(timeout=1)
+    for x in resp:
+        print('\t',x)
 
     i+=1
